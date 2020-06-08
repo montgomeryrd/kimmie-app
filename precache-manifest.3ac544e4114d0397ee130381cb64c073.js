e3cbf0ed620563a382439c4ef2a7b30c2034ed7c
montgomeryrd <montgomery_rd@yahoo.com>
2020-06-08 15:07:46 -0700

    Updates

---
+++ precache-manifest.3ac544e4114d0397ee130381cb64c073.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7225735e10b65fc69b85b94f1c597b75",
+    "url": "/kimmie-app/index.html"
+  },
+  {
+    "revision": "76e0c8d19b775ead3228",
+    "url": "/kimmie-app/static/css/main.c39269bd.chunk.css"
+  },
+  {
+    "revision": "65d9c9ecd3b1bdd91573",
+    "url": "/kimmie-app/static/js/2.83ac2a41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kimmie-app/static/js/2.83ac2a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e0c8d19b775ead3228",
+    "url": "/kimmie-app/static/js/main.2ee262b6.chunk.js"
+  },
+  {
+    "revision": "6234bfe389a94877a224",
+    "url": "/kimmie-app/static/js/runtime-main.c249af6d.js"
+  },
+  {
+    "revision": "d315a363931234d90e1850c6db843259",
+    "url": "/kimmie-app/static/media/add.d315a363.svg"
+  },
+  {
+    "revision": "24a3f9f4314c740a0ccb6c25c482af83",
+    "url": "/kimmie-app/static/media/grocery.24a3f9f4.svg"
+  },
+  {
+    "revision": "c2542a2254281bd71ce315b6086208e5",
+    "url": "/kimmie-app/static/media/like.c2542a22.svg"
+  },
+  {
+    "revision": "e7b4603e0fc420f19b950422d72d7ee5",
+    "url": "/kimmie-app/static/media/shopping-cart.e7b4603e.svg"
+  },
+  {
+    "revision": "e9c5041f46c650ba691b50df38d62d5d",
+    "url": "/kimmie-app/static/media/sunt.e9c5041f.jpg"
+  }
+]);
